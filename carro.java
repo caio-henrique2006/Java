8af25917b265e7a29c0a1ea03661a947b8f3292d
caio-henrique2006 <caio_henrique2006@outlook.com>
2023-01-16 15:17:33 -0300

    .

--- carro.java
+++ carro.java
@@ -30,9 +30,12 @@ class Estrada {
     public static void main(String[] args) {
 
         Motorista Caio = new Motorista("Caio Henrique Almeida Ferreira Santos", "01/06/2006", true);
-        Carro carroCaio = new Carro(Caio, "Pálio", 270, "azul", "ruim", 10);
+        Carro carroCaio = new Carro(Caio, "Pálio", 270, "azul", "bom", 40);
+        Carro carroCaio1 = new Carro(Caio, "Gol", 240, "preto", "bom", 10);
+        //Carro carroCaio2 = new Carro(Caio, "Ford", 260, "vermelho", "bom", 10);
+        Carro carroCaio3 = new Carro(Caio, "Mercedes", 150, "branco", "bom", 5);
 
-        System.out.println(carroCaio.acelerar());
+        System.out.println(Carro.getTotalCarros());
 
     }
 }
@@ -60,15 +63,27 @@ class Carro {
     private double potencia;
     private double velocidadeAtual = 0;
     private double velocidadeMaxima;
+    static int totalCarros;
+    static int quantidadeKilometroRodado;
+
+    Carro(Motorista motorista) {
+        this.motorista = motorista; // Segundo construtor;
+    }
 
     Carro(Motorista motorista, String modelo, double velocidadeMaxima,
     String cor, String estado, double potencia) {
-        this.motorista = motorista;
+        this(motorista); // Chamando o contrutor que foi inicializado acima;
+        Carro.totalCarros += 1; // Somando no atributo static;
         this.modelo = modelo;
         this.velocidadeMaxima = velocidadeMaxima;
         this.cor = cor;
         this.estado = estado;
         this.potencia = potencia;
+    }
+
+    static String getTotalCarros() { // Método static, não é preciso iniciar o construtor para ele funcionar
+        String retorno = "Total de carros adquiridos: " + Carro.totalCarros;
+        return retorno;  // Use Carro.getTotalCarros();
     }
 
     String getCarro() {
@@ -77,25 +92,34 @@ class Carro {
         "\nVelocidade Máxima: " + this.velocidadeMaxima +
         "\nCor: " + this.cor +
         "\nEstado: " + this.estado +
-        "\nPotência: " + this.potencia + " cavalos");
+        "\nPotência: " + this.potencia + " cavalos" +
+        "\nTotal de carros: " + Carro.totalCarros +
+        "\nQuantidade de velocidade rodado: " + Carro.quantidadeKilometroRodado);
         return retorno;
+    }
+
+    String setEstado(String estado) {
+        this.estado = estado;
+        return this.estado;
     }
 
     String acelerar() {
         if(velocidadeAtual < velocidadeMaxima) {
-            this.velocidadeAtual += 10;
-            String retorno = "Velocidade Atual: " + velocidadeAtual;
+            this.velocidadeAtual += this.potencia;
+            Carro.quantidadeKilometroRodado += this.velocidadeAtual; 
+            String retorno = "Velocidade Atual: " + this.velocidadeAtual;
             return retorno;
         } else {
-            String retorno = "Velocidade máxima atingida: ", velocidadeMaxima;
+            String retorno = "Velocidade máxima atingida: " + this.velocidadeMaxima;
             return retorno;
         }
     }
 
     String desacelerar() {
         if(velocidadeAtual > 0) {
-            this.velocidadeAtual -= 10;
-            String retorno = "Velocidade Atual: " + velocidadeAtual;
+            this.velocidadeAtual -= this.potencia;
+            Carro.quantidadeKilometroRodado += this.velocidadeAtual;
+            String retorno = "Velocidade Atual: " + this.velocidadeAtual;
             return retorno;
         } else {
             String retorno = "O carro está parado !!!";
